062f974907195da96f29ef87627652dc01a10dcd
JackDrogon <jack.xsuperman@gmail.com>
2017-01-06 11:42:34 +0800

    Add golang net/http hello sample

---
+++ go/library/std/net/http/hello_http.go
@@ -0,0 +1,17 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hello, %s.", req.URL.Path[1:])
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", mux)
+}
