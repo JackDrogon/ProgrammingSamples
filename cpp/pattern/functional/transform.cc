fe2f210f78a134a64f393bda38728b0fd3f2d1de
JackDrogon <jack.xsuperman@gmail.com>
2017-01-13 17:58:09 +0800

    Add cpp transform sample

---
+++ cpp/pattern/functional/transform.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	string s("hello");
+	transform(s.begin(), s.end(), s.begin(),
+		       [](unsigned char c) { return toupper(c); });
+	cout << s << endl;;
+
+	int a[5] = {0, 1, 2, 3, 4};
+	transform(begin(a), end(a), begin(a), [](int a) { return a+1; });
+	cout << a[0] << endl;
+}
