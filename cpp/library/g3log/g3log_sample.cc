e87cc01bac3f5b6385729bfd1f3a1cd278a0e168
Jack Drogon <jack.xsuperman@gmail.com>
2017-08-25 20:10:26 +0800

    Add g3log samples

---
+++ cpp/library/g3log/g3log_sample.cc
@@ -0,0 +1,11 @@
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+int main()
+{
+	using namespace g3;
+	auto worker = LogWorker::createLogWorker();
+	worker->addDefaultLogger("test", "log");
+	g3::initializeLogging(worker.get());
+	LOG(DEBUG) << "Hello World";
+	return 0;
+}
