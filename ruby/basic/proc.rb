a35ed685efadb7654885a179c2773166713fb34a
Jack Drogon <jack.xsuperman@gmail.com>
2017-10-04 19:49:28 +0800

    Update ruby proc example

--- ruby/basic/proc.rb
+++ ruby/basic/proc.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby
 # -*- coding:utf-8 -*-
 
-multiply = -> x, y { x * y }
-puts multiply.call(6, 9)
-puts multiply[6, 9]
+require 'test/unit'
+
+class ProcTest < Test::Unit::TestCase
+
+  def test_proc
+    multiply = -> x, y { x * y }
+
+    assert_equal(multiply.call(6, 9), 54)
+    assert_equal(multiply[6, 9], 54)
+  end
+
+end
+
