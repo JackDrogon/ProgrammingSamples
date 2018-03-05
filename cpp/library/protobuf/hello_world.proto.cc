7b45e47206b48c5c324389d1426ead4aec6119f0
Jack Drogon <jack.xsuperman@gmail.com>
2018-03-05 16:13:34 +0800

    Add cpp pb dispatcher

--- cpp/library/protobuf/hello_world.proto.cc
+++ cpp/library/protobuf/hello_world.proto.cc
@@ -1,15 +1,16 @@
+#include "hello_world.pb.h"
+#include <functional>
 #include <iostream>
 #include <map>
-#include <functional>
-#include "hello_world.pb.h"
 
 using namespace std;
 
-
-class ProtobufDispatch {
+class ProtobufDispatch
+{
 public:
-	template<typename MessageType>
-	void register_callback(function<void (::google::protobuf::Message*)> callback)
+	template <typename MessageType>
+	void register_callback(
+	    function<void(::google::protobuf::Message *)> callback)
 	{
 		callbacks_[MessageType::descriptor()] = callback;
 	}
@@ -20,28 +21,39 @@ public:
 			callbacks_[msg->GetDescriptor()](msg);
 		}
 	}
+
 private:
-	map<const ::google::protobuf::Descriptor*, function<void (::google::protobuf::Message*)> > callbacks_;
+	map<const ::google::protobuf::Descriptor *,
+	    function<void(::google::protobuf::Message *)>>
+	    callbacks_;
 };
 
 void DealFoo(::google::protobuf::Message *m)
 {
 	hello_world::foo *msg = static_cast<hello_world::foo *>(m);
-	cout << "hello_world::foo => id: " << msg->id() << ", str:" << msg->str() << ", opt:" << msg->opt() << endl;
+	cout << "hello_world::foo => id: " << msg->id()
+	     << ", str:" << msg->str() << ", opt:" << msg->opt() << endl;
 }
 
 void DealBar(::google::protobuf::Message *m)
 {
 	hello_world::bar *msg = static_cast<hello_world::bar *>(m);
-	cout << "hello_world::bar => age: " << msg->age() << ", name:" << msg->name() << endl;
+	cout << "hello_world::bar => age: " << msg->age()
+	     << ", name:" << msg->name() << endl;
 }
 
-::google::protobuf::Message *create_message(const string &type_name, const string &s)
+::google::protobuf::Message *create_message(const string &type_name,
+					    const string &s)
 {
 	::google::protobuf::Message *message = NULL;
-	const ::google::protobuf::Descriptor *descriptor = ::google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName(type_name);
+	const ::google::protobuf::Descriptor *descriptor =
+	    ::google::protobuf::DescriptorPool::generated_pool()
+		->FindMessageTypeByName(type_name);
 	if (descriptor) {
-		const ::google::protobuf::Message *prototype = ::google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+		// const ::google::protobuf::Message*
+		auto prototype =
+		    ::google::protobuf::MessageFactory::generated_factory()
+			->GetPrototype(descriptor);
 		if (prototype) {
 			message = prototype->New();
 		}
