257f9966b5aa36a7aa6ed0d441344d3294b7887e
Jack Drogon <jack.xsuperman@gmail.com>
2018-03-05 16:23:22 +0800

    Update test.asm

--- asm/basic/test/test.asm
+++ asm/basic/test/test.asm
@@ -1,5 +1,9 @@
 global start
 
+section .data
+msg:    db      "Hello, World!", 10
+.len:   equ     $ - msg
+
 section .text
 start:
   mov     rax, 0x2000004 ; write
@@ -7,10 +11,7 @@ start:
   mov     rsi, qword msg
   mov     rdx, msg.len
   syscall
+
   mov     rax, 0x2000001 ; exit
   mov     rdi, 0
   syscall
-
-section .data
-msg:    db      "Hello, World!", 10
-.len:   equ     $ - msg
