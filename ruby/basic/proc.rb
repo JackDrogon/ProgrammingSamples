d4a0c0c623e3c1cbcdac4910896bac07bc001c31
Jack Drogon <jack.xsuperman@gmail.com>
2018-03-05 16:10:31 +0800

    Update ruby proc

--- ruby/basic/proc.rb
+++ ruby/basic/proc.rb
@@ -9,12 +9,11 @@ class ProcTest < Test::Unit::TestCase
     return Proc.new {|n| n*factor}
   end
 
-  def test_proc
+  def test_proc_new
     # multiply = -> x, y { x * y }
     multiply = Proc.new {|x, y| x*y}
 
     assert_equal(multiply.call(6, 9), 54)
-    assert_equal(multiply[6, 9], 54)
   end
 
   def test_proc_with_local_variable
@@ -22,6 +21,19 @@ class ProcTest < Test::Unit::TestCase
 
     assert_equal(multiply_3.call(3), 9)
     assert_equal(multiply_3[3], 9)
+  end
+
+  def test_kernel_proc
+    assert_equal(proc {|a,b| [a,b] }.(1, 2), Kernel.proc {|a, b| [a, b]}.(1, 2))
+  end
+
+  def test_proc_call_methods
+    multiply = Proc.new {|x, y| x*y}
+
+    assert_equal(multiply.call(6, 9), 54)
+    assert_equal(multiply[6, 9], 54)
+    assert_equal(multiply.(6, 9), 54)
+    assert_equal(multiply.yield(6, 9), 54)
   end
 
 end
