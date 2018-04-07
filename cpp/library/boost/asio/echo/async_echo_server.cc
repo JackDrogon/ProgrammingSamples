332245602a7c2561b4fa38f2e8c76394ad0ffb22
Jack Drogon <jack.xsuperman@gmail.com>
2018-04-07 12:50:50 +0800

    Refactor async_echo_server.c
    Using explicit for ctor, another private mark for data members.
    Using io_context(1) for avoiding lock usage.

--- cpp/library/boost/asio/echo/async_echo_server.cc
+++ cpp/library/boost/asio/echo/async_echo_server.cc
@@ -2,71 +2,78 @@
 #include <iostream>
 #include <memory>
 #include <utility>
+
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-class session : public std::enable_shared_from_this<session>
+class Session : public std::enable_shared_from_this<Session>
 {
 public:
-	session(tcp::socket socket) : socket_(std::move(socket)) {}
+	explicit Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-	void start() { do_read(); }
+	void Start() { read(); }
 
 private:
-	void do_read()
+	void read()
 	{
 		auto self(shared_from_this());
+
 		socket_.async_read_some(
-		    boost::asio::buffer(data_, max_length),
+		    boost::asio::buffer(data_, kMaxLength),
 		    [this, self](boost::system::error_code ec,
 				 std::size_t length) {
 			    if (!ec) {
-				    do_write(length);
+				    write(length);
 			    }
 		    });
 	}
 
-	void do_write(std::size_t length)
+	void write(std::size_t length)
 	{
 		auto self(shared_from_this());
+
 		boost::asio::async_write(
 		    socket_, boost::asio::buffer(data_, length),
 		    [this, self](boost::system::error_code ec,
 				 std::size_t /*length*/) {
 			    if (!ec) {
-				    do_read();
+				    read();
 			    }
 		    });
 	}
 
+private:
+	enum { kMaxLength = 1024 };
+	char data_[kMaxLength];
+
 	tcp::socket socket_;
-	enum { max_length = 1024 };
-	char data_[max_length];
 };
 
-class server
+class Server
 {
 public:
-	server(boost::asio::io_service &io_service, short port)
-	    : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-	      socket_(io_service)
+	explicit Server(boost::asio::io_context &io_context,
+			unsigned short port)
+	    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)),
+	      socket_(io_context)
 	{
-		do_accept();
 	}
 
+	void Start() { accept(); }
+
 private:
-	void do_accept()
+	void accept()
 	{
 		acceptor_.async_accept(
 		    socket_, [this](boost::system::error_code ec) {
 			    if (!ec) {
-				    std::make_shared<session>(
+				    std::make_shared<Session>(
 					std::move(socket_))
-					->start();
+					->Start();
 			    }
 
-			    do_accept();
+			    accept();
 		    });
 	}
 
@@ -82,11 +89,13 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 
-		boost::asio::io_service io_service;
+		boost::asio::io_context io_context(1);
 
-		server s(io_service, std::atoi(argv[1]));
+		Server server(io_context,
+			      static_cast<unsigned short>(std::atoi(argv[1])));
+		server.Start();
 
-		io_service.run();
+		io_context.run();
 	} catch (std::exception &e) {
 		std::cerr << "Exception: " << e.what() << "\n";
 	}
