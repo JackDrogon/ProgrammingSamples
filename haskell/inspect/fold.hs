478d0886b047242a219ae62d82d5b10fadaee5cb
Jack Drogon <jack.xsuperman@gmail.com>
2018-06-01 19:18:27 +0800

    Add haskell fold inspect

--- haskell/inspect/fold.hs
+++ haskell/inspect/fold.hs
@@ -1,5 +1,6 @@
-foldr' f acc [] = acc
-foldr' f acc h:t = foldr' f h (foldr' f acc t)
+module Inspect where
+    foldr' f acc [] = acc
+    foldr' f acc (h:t) = f h (foldr' f acc t)
 
-foldl' f acc [] = acc
-foldl' f acc h:t = foldl' f (f acc h) t
+    foldl' f acc [] = acc
+    foldl' f acc (h:t) = foldl' f (f acc h) t
