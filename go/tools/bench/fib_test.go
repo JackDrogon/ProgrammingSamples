f9e1bd3756f07c9df5e5126d21ffedcea9066fca
Jack Drogon <jack.xsuperman@gmail.com>
2018-06-02 12:02:34 +0800

    Update go bench sample

--- go/tools/bench/fib_test.go
+++ go/tools/bench/fib_test.go
@@ -2,9 +2,25 @@ package fib
 
 import "testing"
 
+func BenchmarkFib3(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(3)
+	}
+}
+
 func BenchmarkFib10(b *testing.B) {
 	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
 		Fib(10)
 	}
 }
+
+func benchmarkFib(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(i)
+	}
+}
+func BenchmarkFib1(b *testing.B)  { benchmarkFib(1, b) }
+func BenchmarkFib2(b *testing.B)  { benchmarkFib(2, b) }
+func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
+func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
