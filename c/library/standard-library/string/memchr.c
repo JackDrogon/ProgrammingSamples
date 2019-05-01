3db414d8f3c1555da1a0075cc956a24924fb0f80
Jack Drogon <jack.xsuperman@gmail.com>
2019-05-01 10:12:13 +0800

    Add c memchr

---
+++ c/library/standard-library/string/memchr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *s = "Hello, World!";
+	char *p;
+	p = (char *)memchr(s, 'W', sizeof(s));
+	if (p)
+		puts(p);
+	else
+		puts("Not Found");
+
+	return 0;
+}
