4b6c1ee2cca0ef9dd1a8274f6d48cf3c8500a679
Jack Drogon <jack.xsuperman@gmail.com>
2019-05-07 12:13:52 +0800

    Add cpp set sample

---
+++ cpp/library/standard-library/set/set_sample.cc
@@ -0,0 +1,26 @@
+#include <set>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	set<string> cities{"Braunschweig", "Hanover",  "Frankfurt",
+				"New York",     "Chicago",  "Toronto",
+				"Paris",	"Frankfurt"};
+
+	// print each element:
+	for (const auto &elem : cities) {
+		cout << elem << "  ";
+	}
+	cout << endl;
+
+	// insert additional values:
+	cities.insert({"London", "Munich", "Hanover", "Braunschweig"});
+
+	// print each element:
+	for (const auto &elem : cities) {
+		cout << elem << "  ";
+	}
+	cout << endl;
+}
