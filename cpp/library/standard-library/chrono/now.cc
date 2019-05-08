9e05e7990bf10d9783ea6cee2e4c1aa9923d94e6
Jack Drogon <jack.xsuperman@gmail.com>
2019-05-08 15:21:18 +0800

    Add cpp chrono now sample

---
+++ cpp/library/standard-library/chrono/now.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <chrono>
+
+int main()
+{
+	volatile int sink;
+	for (auto size = 1ull; size < 1000000000ull; size *= 100) {
+		// record start time
+		auto start = std::chrono::steady_clock::now();
+		// do some work
+		std::vector<int> v(size, 42);
+		sink = std::accumulate(v.begin(), v.end(),
+				       0u); // make sure it's a side effect
+		// record end time
+		auto end = std::chrono::steady_clock::now();
+		std::chrono::duration<double> diff = end - start;
+		std::cout << "Time to fill and iterate a vector of " << size
+			  << " ints : " << diff.count() << " s\n";
+	}
+}
