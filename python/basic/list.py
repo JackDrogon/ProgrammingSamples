da7efe3095e60d7bf2c1f7afc130f7f21818c0ae
Jack Drogon <jack.xsuperman@gmail.com>
2019-06-23 10:05:13 +0800

    Add python3 list sample

---
+++ python/basic/list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from pprint import pprint
+from run_decorator import runf
+
+
+@runf
+def list_t():
+    pprint([1, 2, 3])
+
+
+def main():
+    list_t()
+
+
+if __name__ == "__main__": main()
