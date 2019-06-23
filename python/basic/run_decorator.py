1bd6e91794206acc37d5a9ad6a9652db87210bbc
Jack Drogon <jack.xsuperman@gmail.com>
2019-06-23 10:03:45 +0800

    Update python3 run_decorator with __main__

--- python/basic/run_decorator.py
+++ python/basic/run_decorator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-def run(func):
-    print("Run function: {}".format(func.__name__))
+def runf(func):
     def wrapper_func(*args):
+        print("Run function: {}".format(func.__name__))
         result = func(*args)
         print("---------------------------------------------------------")
         return result
     return wrapper_func
 
-@run
+@runf
 def func(parms):
     print(parms)
 
-func("Hello")
+if __name__ == "__main__": func("Hello")
