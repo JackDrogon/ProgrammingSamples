44f1f2b9058f61351e01959fb5078e11bcce4743
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-03 07:45:51 +0800

    Add c11 static_assert sample

---
+++ c/library/standard-library/assert/static_assert.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+
+int main(void)
+{
+    static_assert(2 + 2 == 4, "2+2 isn't 4");      // well-formed
+    static_assert(sizeof(int) < sizeof(char),
+                 "this program requires that int is less than char"); // compile-time error
+}
