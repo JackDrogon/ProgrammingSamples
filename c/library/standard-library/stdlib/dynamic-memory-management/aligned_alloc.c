1d4412e1a9b2f93087fa823e847f9ada6ebdedf3
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-04 11:10:18 +0800

    Add c11 aligned_alloc sample

---
+++ c/library/standard-library/stdlib/dynamic-memory-management/aligned_alloc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int *p1 = malloc(10 * sizeof *p1);
+	printf("default-aligned addr:   %p\n", (void *)p1);
+	free(p1);
+
+	int *p2 = aligned_alloc(1024, 1024 * sizeof *p2);
+	printf("1024-byte aligned addr: %p\n", (void *)p2);
+	free(p2);
+}
