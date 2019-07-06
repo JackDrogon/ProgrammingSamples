e22545031f7a7b4b38feb27596e72b84adf13a81
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-06 09:35:52 +0800

    Add python3 asyncio sample

---
+++ python/library/standard-library/asyncio/asyncio_simple2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+if __name__ == "__main__": asyncio.run(main())
