4f925e15fa9530ce79818fa8e6311e39023bee92
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-07 10:20:15 +0800

    Add python3 asyncio create_task

---
+++ python/library/standard-library/asyncio/asyncio_create_task.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    task1 = asyncio.create_task(say_after(1, "hello"))
+    task2 = asyncio.create_task(say_after(2, "world"))
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    print(f"started at {time.strftime('%X')}")
+    await task1
+    await task2
+    print(f"finished at {time.strftime('%X')}")
+
+
+if __name__ == "__main__": asyncio.run(main())
