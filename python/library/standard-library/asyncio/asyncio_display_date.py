a5bc8ba3354f86bcadbe7566a5ed0d55d3b3b666
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-08 08:17:16 +0800

    Add python3 asyncio show datetime sample

---
+++ python/library/standard-library/asyncio/asyncio_display_date.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import asyncio
+import datetime
+
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if loop.time() + 1.0 >= end_time:
+            break
+        await asyncio.sleep(1)
+
+if __name__ == "__main__": asyncio.run(display_date())
