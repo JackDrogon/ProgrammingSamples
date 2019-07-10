4361baf2f1895a90f411449af0750d69dc24adcc
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-10 10:13:55 +0800

    Add python3 string some constants

---
+++ python/library/standard-library/string/string_constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import string
+
+print(string.ascii_lowercase)  # python2 string.lowercase
+print(string.ascii_uppercase)
+print(string.ascii_letters)
+print(string.digits)
