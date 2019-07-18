c05584f875395e3a78d4999f4eecd0a93a7436c8
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-18 10:05:11 +0800

    Update go echo_server

--- go/library/standard-library/net/echo_server.go
+++ go/library/standard-library/net/echo_server.go
@@ -2,77 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
-	"os"
-	"time"
 )
 
-const (
-	MAX_CONN_NUM = 5
-)
-
-//echo server Goroutine
-func echoFunc(conn net.Conn) {
+func handleConn(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	for {
-		_, err := conn.Read(buf)
-		if err != nil {
-			//println("Error reading:", err.Error())
-			return
-		}
-		//send reply
-		_, err = conn.Write(buf)
-		if err != nil {
-			//println("Error send reply:", err.Error())
-			return
-		}
+
+	io.Copy(conn, conn)
+}
+
+type EchoServer struct {
+	address string
+}
+
+func NewEchoServer(address string) *EchoServer {
+	return &EchoServer{
+		address: address,
 	}
 }
 
-//initial listener and run
-func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:8088")
+func (srv *EchoServer) Run() {
+	listener, err := net.Listen("tcp", srv.address)
 	if err != nil {
-		fmt.Println("error listening:", err.Error())
-		os.Exit(1)
+		fmt.Println("Listen error:", err)
+		return
 	}
 	defer listener.Close()
-
-	fmt.Printf("running, listen on 0.0.0.0:8088 ...\n")
-
-	var cur_conn_num int = 0
-	conn_chan := make(chan net.Conn)
-	ch_conn_change := make(chan int)
-
-	go func() {
-		for conn_change := range ch_conn_change {
-			cur_conn_num += conn_change
-		}
-	}()
-
-	go func() {
-		for _ = range time.Tick(1e8) {
-			fmt.Printf("cur conn num: %d\n", cur_conn_num)
-		}
-	}()
-
-	for i := 0; i < MAX_CONN_NUM; i++ {
-		go func() {
-			for conn := range conn_chan {
-				ch_conn_change <- 1
-				echoFunc(conn)
-				ch_conn_change <- -1
-			}
-		}()
-	}
+	println("Listen on", srv.address)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			println("Error accept:", err.Error())
-			return
+			fmt.Println("Accept error:", err)
+			continue
 		}
-		conn_chan <- conn
+
+		go handleConn(conn)
 	}
+}
+
+func main() {
+	srv := NewEchoServer("127.0.0.1:8888")
+	srv.Run()
 }
