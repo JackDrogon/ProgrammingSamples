c5f74a767c08d3066058f806edce7d5189c3b9b7
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-20 09:44:51 +0800

    Add go bolt open/write sample

---
+++ go/library/extend-library/boltdb/boltdb_write.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+func main() {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("botltdb", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("posts"))
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte("2015-01-01"), []byte("My New Year post"))
+	})
+}
