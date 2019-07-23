8381b26947346a6283c20722616e949b641ea8dd
Jack Drogon <jack.xsuperman@gmail.com>
2019-07-23 09:41:17 +0800

    Add go inject sample

---
+++ go/library/extend-library/inject/inject.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/codegangsta/inject"
+)
+
+type SpecialString interface{}
+
+func Say(name string, gender SpecialString, age int) {
+	fmt.Printf("My name is %s, gender is %s, age is %d!\n", name, gender, age)
+}
+
+func main() {
+	inj := inject.New()
+	inj.Map("陈一回")
+	inj.MapTo("男", (*SpecialString)(nil))
+	inj.Map(20)
+	inj.Invoke(Say)
+}
