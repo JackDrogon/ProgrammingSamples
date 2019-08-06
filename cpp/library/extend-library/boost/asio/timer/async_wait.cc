d553729f1a6092e0465b5340757b0127ff8a2d41
Jack Drogon <jack.xsuperman@gmail.com>
2019-08-06 08:36:23 +0800

    Add cpp boost asio timer async_wait sample

---
+++ cpp/library/extend-library/boost/asio/timer/async_wait.cc
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <iostream>
+using namespace std;
+using namespace std::chrono_literals;
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+
+int main()
+{
+	boost::asio::io_context io_context;
+	boost::asio::steady_timer timer(io_context, 1s);
+
+	{
+		std::chrono::steady_clock::time_point start =
+		    std::chrono::steady_clock::now();
+		timer.async_wait([=](const boost::system::error_code &) {
+			cout << "Run timer completion function, distance "
+			     << std::chrono::duration_cast<
+				    std::chrono::milliseconds>(
+				    std::chrono::steady_clock::now() - start)
+				    .count()
+			     << "us" << endl;
+		});
+	}
+	io_context.run();
+
+	return 0;
+}
