55854157868025009784bd6886649892200d5813
Jack Drogon <jack.xsuperman@gmail.com>
2019-08-19 10:11:12 +0800

    Add cpp boost asio spawn parallel_grep sample

---
+++ cpp/library/extend-library/boost/asio/spawn/README.md
@@ -0,0 +1,6 @@
+## Build
+
+1. parallel_grep
+```sh
+g++ -std=c++1z -Weverything -Wno-c++98-compat -o parallel_grep ./parallel_grep.cc -lboost_coroutine
+```
---
+++ cpp/library/extend-library/boost/asio/spawn/parallel_grep.cc
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/post.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/asio/thread_pool.hpp>
+
+int main(int argc, char *argv[])
+{
+	try {
+		if (argc < 2) {
+			cerr << "Usage: " << argv[0] << " <string> <files...>"
+			     << endl;
+			return EXIT_FAILURE;
+		}
+
+		boost::asio::thread_pool thread_pool;
+		boost::asio::strand<boost::asio::thread_pool::executor_type>
+		    output_strand(thread_pool.get_executor());
+		string search_string = argv[1];
+		for (int argn = 2; argn < argc; ++argn) {
+			string input_file = argv[argn];
+			boost::asio::spawn(
+			    thread_pool, [=](boost::asio::yield_context yield) {
+				    std::ifstream is(input_file.c_str());
+				    string line;
+				    size_t line_num = 0;
+				    while (getline(is, line)) {
+					    ++line_num;
+					    if (line.find(search_string) ==
+						string::npos) {
+						    continue;
+					    }
+					    boost::asio::dispatch(
+						output_strand, [=] {
+							cout << input_file
+							     << ":" << line_num
+							     << " " << line
+							     << endl;
+						});
+					    if (line_num % 10 == 0) {
+						    post(yield);
+					    }
+				    }
+			    });
+		}
+		thread_pool.join();
+	} catch (exception &e) {
+		cerr << "Exception: " << e.what() << endl;
+	}
+	return 0;
+}
