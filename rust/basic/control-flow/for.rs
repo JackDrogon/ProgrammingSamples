fc2d15071809d6459f47b4739f3a9a42b5e88d0b
Jack Drogon <jack.xsuperman@gmail.com>
2019-08-22 07:13:19 +0800

    Add rust control flow for sample

---
+++ rust/basic/control-flow/for.rs
@@ -0,0 +1,10 @@
+pub fn main() {
+    for (i,j) in (5..10).enumerate() {
+        println!("i = {} and j = {}", i, j);
+    }
+
+    let lines = "hello\nworld".lines();
+    for (linenumber, line) in lines.enumerate() {
+        println!("{}: {}", linenumber, line);
+    }
+}
