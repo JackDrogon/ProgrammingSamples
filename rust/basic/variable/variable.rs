d8b9d4b1abb2f88bfbd8b5ff48ebe0d6c8844c2e
Jack Drogon <jack.xsuperman@gmail.com>
2019-08-28 09:10:38 +0800

    Add rust variable sample

---
+++ rust/basic/variable/variable.rs
@@ -0,0 +1,6 @@
+pub fn main() {
+    let x = 30;
+    let mut y = 15;
+    y = 13;
+    println!("x: {}, y: {}", x, y)
+}
---
+++ rust/basic/variable/variable_bindings.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let (x, y) = (1, 2);
+    println!("x: {}, y: {}.", x, y);
+}
