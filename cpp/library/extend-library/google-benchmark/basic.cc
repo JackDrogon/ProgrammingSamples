e7c7056664125c2e8065d8d68661a8c2642e4332
Jack Drogon <jack.xsuperman@gmail.com>
2019-09-03 08:15:51 +0800

    Update cpp google-benchmark basic.cc with `{}`

--- cpp/library/extend-library/google-benchmark/basic.cc
+++ cpp/library/extend-library/google-benchmark/basic.cc
@@ -2,8 +2,9 @@
 
 static void BM_StringCreation(benchmark::State &state)
 {
-	while (state.KeepRunning())
+	while (state.KeepRunning()) {
 		std::string empty_string;
+	}
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
@@ -12,8 +13,9 @@ BENCHMARK(BM_StringCreation);
 static void BM_StringCopy(benchmark::State &state)
 {
 	std::string x = "hello";
-	while (state.KeepRunning())
+	while (state.KeepRunning()) {
 		std::string copy(x);
+	}
 }
 BENCHMARK(BM_StringCopy);
 
