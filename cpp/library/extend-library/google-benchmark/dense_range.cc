b29ad0a1e34e4b01473cee806ef176619790147d
Jack Drogon <jack.xsuperman@gmail.com>
2019-09-06 09:10:56 +0800

    Add cpp google benchmark dense range sample

---
+++ cpp/library/extend-library/google-benchmark/dense_range.cc
@@ -0,0 +1,12 @@
+#include <benchmark/benchmark.h>
+
+static void BM_DenseRange(benchmark::State& state) {
+  for(auto _ : state) {
+    std::vector<int> v(state.range(0), state.range(0));
+    benchmark::DoNotOptimize(v.data());
+    benchmark::ClobberMemory();
+  }
+}
+BENCHMARK(BM_DenseRange)->DenseRange(0, 1024, 128);
+
+BENCHMARK_MAIN();
