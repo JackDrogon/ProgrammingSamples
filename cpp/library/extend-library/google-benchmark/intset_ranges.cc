d84ee1f7ee2be0ecbe83ef310788dfcae660d166
Jack Drogon <jack.xsuperman@gmail.com>
2019-09-07 08:51:11 +0800

    Add cpp google benchmark ranges sample

---
+++ cpp/library/extend-library/google-benchmark/intset_ranges.cc
@@ -0,0 +1,41 @@
+#include <random>
+#include <set>
+using namespace std;
+
+#include <benchmark/benchmark.h>
+
+std::set<int64_t> ConstructRandomSet(int64_t size)
+{
+	std::set<int64_t> s;
+	for (int i = 0; i < size; ++i) {
+		s.insert(s.end(), i);
+	}
+	return s;
+}
+
+static int64_t RandomNumber()
+{
+	static std::mt19937 rd{std::random_device{}()};
+
+	return rd();
+}
+
+// You might have a microbenchmark that depends on two inputs.  For
+// example, the following code defines a family of microbenchmarks for
+// measuring the speed of set insertion.
+static void BM_SetInsert(benchmark::State &state)
+{
+	set<int64_t> data;
+	for (auto _ : state) {
+		state.PauseTiming();
+		data = ConstructRandomSet(state.range(0));
+		state.ResumeTiming();
+		for (int j = 0; j < state.range(1); ++j) {
+			data.insert(RandomNumber());
+		}
+	}
+}
+
+BENCHMARK(BM_SetInsert)->Ranges({{1 << 10, 8 << 10}, {128, 512}});
+
+BENCHMARK_MAIN();
