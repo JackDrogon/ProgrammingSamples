8b939f1c7ea2c290ebb0b5e04ea97bfda9e4f2ee
Jack Drogon <jack.xsuperman@gmail.com>
2019-09-25 08:27:19 +0800

    Add rust net echo server sample

---
+++ rust/library/standard-library/net/echo_server.rs
@@ -0,0 +1,40 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Read;
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    // read 20 bytes at a time from stream echoing back to stream
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 { 
+                    // connection was closed
+                    break;
+                }
+                stream.write(&read[0..n]).unwrap();
+            }
+            Err(err) => {
+                panic!(err);
+            }
+        }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+    }
+}
