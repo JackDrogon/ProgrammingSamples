c77e60f03a32164e16ca2ee33411993166a96ae3
Jack Drogon <jack.xsuperman@gmail.com>
2019-10-22 07:20:18 +0800

    Update go clock.go with log

--- go/basic/test/concurrent/clock.go
+++ go/basic/test/concurrent/clock.go
@@ -8,29 +8,33 @@ import (
 	"time"
 )
 
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	address := "localhost:8000"
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Printf("Listen on %s\n", address)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Print(err) // e.g., connection aborted
+			log.Print(err)
 			continue
 		}
-		handleConn(conn) // handle one connection at a time
-	}
-}
 
-func handleConn(c net.Conn) {
-	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return // e.g., client disconnected
-		}
-		time.Sleep(1 * time.Second)
+		handleConn(conn)
 	}
 }
