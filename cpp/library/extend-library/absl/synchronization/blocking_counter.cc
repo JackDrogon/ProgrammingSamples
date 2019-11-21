9494ac950bb0d43a2d9b179413199b3f5754a1d1
Jack Drogon <jack.xsuperman@gmail.com>
2019-11-21 09:15:51 +0800

    Update cpp absl synchronization BlockingCounter sample

--- cpp/library/extend-library/absl/synchronization/blocking_counter.cc
+++ cpp/library/extend-library/absl/synchronization/blocking_counter.cc
@@ -7,19 +7,30 @@ using namespace std;
 
 namespace
 {
-constexpr auto kCounter = 10;
+constexpr size_t kCounter = 10;
 }
 
 int main()
 {
 	absl::BlockingCounter wg(kCounter);
 	vector<thread> threads;
+	vector<bool> done(kCounter, false);
+	threads.reserve(kCounter);
 
-	for (int i = 0; i < kCounter; ++i) {
-		threads.emplace_back([&wg] { wg.DecrementCount(); });
+	for (size_t i = 0; i < kCounter; ++i) {
+		threads.emplace_back([&wg, done = &done[i]] {
+			*done = true;
+			wg.DecrementCount();
+		});
 	}
 	wg.Wait();
 	cout << "Counter end to 0" << endl;
+
+	for (auto &&d : done) {
+		if (d != true) {
+			cerr << "Thread run done is false" << endl;
+		}
+	}
 
 	for (auto &thread : threads) {
 		if (thread.joinable()) {
