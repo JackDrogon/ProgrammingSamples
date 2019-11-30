21656168ec41e92f8d7ad1231b903a9e4dfa5858
Jack Drogon <jack.xsuperman@gmail.com>
2019-11-30 09:12:35 +0800

    Update cpp codebase defer_test

--- cpp/codebase/common/defer_test.cc
+++ cpp/codebase/common/defer_test.cc
@@ -1,10 +1,56 @@
 #include "defer.hh"
 
+#include <functional>
 #include <iostream>
 using namespace std;
 
+namespace
+{
+
+void valid()
+{
+	int n = 0;
+	cout << "Before defer: n = " << n << endl;
+	{
+		defer[&n] { ++n; };
+	}
+	cout << "After defer: n = " << n << endl;
+}
+
+void check_lambda()
+{
+	defer[] { cout << "defer lambda call" << endl; };
+}
+
+void bind_f() { cout << "defer bind_f call" << endl; }
+void check_bind() { defer bind(bind_f); }
+
+void check_opeator()
+{
+	struct S {
+		void operator()() { cout << "defer operator() call" << endl; }
+	};
+	defer S();
+}
+
+// TODO
+// void check_class_function()
+// {
+// 	struct S {
+// 		void call() { cout << "defer operator() call" << endl; }
+// 	};
+// 	defer S().call();
+// }
+
+} // namespace
+
 int main()
 {
-	defer [] { cout << "defer call" << endl; };
+	valid();
+
+	check_lambda();
+	check_bind();
+	check_opeator();
+
 	return 0;
 }
