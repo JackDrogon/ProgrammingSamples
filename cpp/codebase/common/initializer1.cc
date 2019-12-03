3917e0e24f794b1c8830039ec0a1909383ed122f
Jack Drogon <jack.xsuperman@gmail.com>
2019-12-03 08:25:17 +0800

    Add cpp codebase initializer multi files test

--- cpp/codebase/common/initializer1.cc
+++ cpp/codebase/common/initializer1.cc
@@ -4,7 +4,15 @@ using namespace std;
 #include "initializer.hh"
 using namespace archimedes::common;
 
-static int n{0};
+static auto dummy = Initializer::Register(
+    "hello",
+    [] {
+	    cout << "another high" << endl;
+	    return true;
+    },
+    Initializer::Priority::kHigh);
+
+static int n{10};
 static auto dummy1 = Initializer::Register("hello", [n = ++n] {
 	cout << "normal: " << n << endl;
 	return true;
@@ -17,17 +25,3 @@ static auto dummy3 = Initializer::Register("hello", [n = ++n] {
 	cout << "normal: " << n << endl;
 	return true;
 });
-
-static auto dummy = Initializer::Register(
-    "hello",
-    [n = ++n] {
-	    cout << "high: " << n << endl;
-	    return true;
-    },
-    Initializer::Priority::kHigh);
-
-int main()
-{
-	RUN_INIT();
-	return 0;
-}
