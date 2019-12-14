b0c614db99c0b031b720b13dc5063c0a8158e666
Jack Drogon <jack.xsuperman@gmail.com>
2019-12-14 09:22:54 +0800

    Refacotr cpp codebase string_utils.hh
    - Refacotr spilt: auto/size_t, unfold if
    - Comment trim

--- cpp/codebase/string/string_utils.hh
+++ cpp/codebase/string/string_utils.hh
@@ -10,18 +10,20 @@ template <typename StringContainer = std::string_view>
 [[nodiscard]] auto split(const std::string_view &str, const char delimiter)
 {
 	std::vector<StringContainer> result;
-	auto beg = 0, end = 0;
+	size_t beg = 0, end = 0;
 
 	for (auto ch : str) {
 		++end;
-		if (ch == delimiter) {
-			if (beg != end - 1) {
-				result.emplace_back(str.data() + beg,
-						    end - 1 - beg);
-			}
-			beg = end;
+		if (ch != delimiter) {
+			continue;
 		}
+
+		if (beg != end - 1) {
+			result.emplace_back(str.data() + beg, end - 1 - beg);
+		}
+		beg = end;
 	}
+
 	if (beg != end) {
 		result.emplace_back(str.data() + beg, end - beg);
 	}
@@ -29,6 +31,6 @@ template <typename StringContainer = std::string_view>
 	return result;
 }
 
-auto trim(const std::string *str, const std::string_view &trim_set) {}
+// auto trim(const std::string *str, const std::string_view &trim_set) {}
 
 }; // namespace archimedes::string_utils
