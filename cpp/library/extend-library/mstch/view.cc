110906687808e3e3fc1d9eed2aa0113bab9ac5b5
Jack Drogon <jack.xsuperman@gmail.com>
2019-12-16 10:05:41 +0800

    Add cpp mstch view sample

---
+++ cpp/library/extend-library/mstch/view.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <mstch/mstch.hpp>
+
+int main()
+{
+	std::string view{"{{#names}}Hi {{name}}!\n{{/names}}"};
+	mstch::map context{
+	    {"names", mstch::array{
+			  mstch::map{{"name", std::string{"Chris"}}},
+			  mstch::map{{"name", std::string{"Mark"}}},
+			  mstch::map{{"name", std::string{"Scott"}}},
+		      }}};
+
+	std::cout << mstch::render(view, context) << std::endl;
+
+	return 0;
+}
