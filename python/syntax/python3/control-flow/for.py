06cbd4c64cd459a14fad704daf41348f464ebbb2
Jack Drogon <jack.xsuperman@gmail.com>
2020-02-20 09:14:17 +0800

    Add python python3 for sample

---
+++ python/syntax/python3/control-flow/for.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+
+def main():
+    for animal in ["dog", "cat", "mouse"]:
+        print("{} is a mammal".format(animal))
+
+
+if __name__ == "__main__":
+    main()
