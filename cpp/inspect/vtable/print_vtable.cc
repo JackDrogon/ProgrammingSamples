058e6821f2ebb15f047a0ed58856cdd9fb45e475
Jack Drogon <jack.xsuperman@gmail.com>
2020-03-04 20:19:27 +0800

    Format cpp inspect print_vtable.cc

--- cpp/inspect/vtable/print_vtable.cc
+++ cpp/inspect/vtable/print_vtable.cc
@@ -1,34 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Base1 {
+class Base1
+{
 public:
 	virtual void f() { cout << "Base1::f" << endl; }
 	virtual void g() { cout << "Base1::g" << endl; }
 	virtual void h() { cout << "Base1::h" << endl; }
 };
 
-class Base2 {
+class Base2
+{
 public:
 	virtual void f() { cout << "Base2::f" << endl; }
 	virtual void g() { cout << "Base2::g" << endl; }
 	virtual void h() { cout << "Base2::h" << endl; }
 };
 
-class Base3 {
+class Base3
+{
 public:
 	virtual void f() { cout << "Base3::f" << endl; }
 	virtual void g() { cout << "Base3::g" << endl; }
 	virtual void h() { cout << "Base3::h" << endl; }
 };
 
-class Derive : public Base1, public Base2, public Base3 {
+class Derive : public Base1, public Base2, public Base3
+{
 public:
 	virtual void f() { cout << "Derive::f" << endl; }
 	virtual void g1() { cout << "Derive::g1" << endl; }
 };
 
-typedef void(*Fun)(void);
+typedef void (*Fun)(void);
 
 int main()
 {
