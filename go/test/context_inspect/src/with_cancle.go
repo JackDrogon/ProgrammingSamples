8144cc65a29d3833e7dc220201fe60ef34740f9e
Jack Drogon <jack.xsuperman@gmail.com>
2020-03-27 08:31:29 +0800

    Add go context_inspect test

---
+++ go/test/context_inspect/src/with_cancle.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"contextp"
+	"fmt"
+)
+
+func main() {
+	// gen generates integers in a separate goroutine and
+	// sends them to the returned channel.
+	// The callers of gen need to cancel the context once
+	// they are done consuming generated integers not to leak
+	// the internal goroutine started by gen.
+
+	type favContextKey string
+
+	f := func(ctx contextp.Context, k favContextKey) {
+		if v := ctx.Value(k); v != nil {
+			fmt.Println("found value:", v)
+			return
+		}
+		fmt.Println("key not found:", k)
+	}
+
+	k := favContextKey("language")
+	ctx := contextp.WithValue(contextp.Background(), k, "Go")
+
+	f(ctx, k)
+	f(ctx, favContextKey("color"))
+
+	gen := func(ctx contextp.Context) <-chan int {
+		dst := make(chan int)
+		n := 1
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return // returning not to leak the goroutine
+				case dst <- n:
+					n++
+				}
+			}
+		}()
+		return dst
+	}
+
+	ctx, cancel := contextp.WithCancel(ctx)
+	defer cancel() // cancel when we are finished consuming integers
+
+	for n := range gen(ctx) {
+		fmt.Println(n)
+		if n == 5 {
+			break
+		}
+	}
+}
