14ac57eb91acca3855d28f68ca1a3fb53e77a1b6
Jack Drogon <jack.xsuperman@gmail.com>
2020-05-27 09:15:17 +0800

    [cpp] {library} Add cpp extend-library absl debugging symbolize sample

---
+++ cpp/library/extend-library/absl/debugging/symbolize.cc
@@ -0,0 +1,30 @@
+#include <absl/debugging/symbolize.h>
+#include <absl/strings/str_format.h>
+
+namespace
+{
+void func(int) __attribute__((noinline));
+void func(int) {}
+
+static void DumpPCAndSymbol(void *pc)
+{
+	char tmp[1024];
+	const char *symbol = "(unknown)";
+	if (absl::Symbolize(pc, tmp, sizeof(tmp))) {
+		symbol = tmp;
+	}
+
+	absl::PrintF("%p  %s\n", pc, symbol);
+}
+} // namespace
+
+int main(int argc, char **argv)
+{
+	// Initialize the Symbolizer
+	absl::InitializeSymbolizer(argv[0]);
+
+	// TODO: dump func(int) not '(unknown)'
+	DumpPCAndSymbol((void *)(&func));
+
+	return 0;
+}
