209f79fa6631569c65bd873d736a2c1351e65ed2
Jack Drogon <jack.xsuperman@gmail.com>
2021-09-18 20:05:16 +0800

    Update parallel_grep by removing try && Add some todos

--- cpp/library/extend-library/boost/asio/spawn/README.md
+++ cpp/library/extend-library/boost/asio/spawn/README.md
@@ -4,3 +4,6 @@
 ```sh
 g++ -std=c++1z -Weverything -Wno-c++98-compat -o parallel_grep ./parallel_grep.cc -lboost_coroutine
 ```
+
+TODO
+parallel_grep 的文件输出顺序
--- cpp/library/extend-library/boost/asio/spawn/parallel_grep.cc
+++ cpp/library/extend-library/boost/asio/spawn/parallel_grep.cc
@@ -1,7 +1,8 @@
-#include <cstdlib>
 #include <fstream>
 #include <iostream>
 using namespace std;
+
+#include <cstdlib>
 
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/post.hpp>
@@ -11,46 +12,43 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	try {
-		if (argc < 2) {
-			cerr << "Usage: " << argv[0] << " <string> <files...>"
-			     << endl;
-			return EXIT_FAILURE;
-		}
-
-		boost::asio::thread_pool thread_pool;
-		boost::asio::strand<boost::asio::thread_pool::executor_type>
-		    output_strand(thread_pool.get_executor());
-		string search_string = argv[1];
-		for (int argn = 2; argn < argc; ++argn) {
-			string input_file = argv[argn];
-			boost::asio::spawn(
-			    thread_pool, [=](boost::asio::yield_context yield) {
-				    std::ifstream is(input_file.c_str());
-				    string line;
-				    size_t line_num = 0;
-				    while (getline(is, line)) {
-					    ++line_num;
-					    if (line.find(search_string) ==
-						string::npos) {
-						    continue;
-					    }
-					    boost::asio::dispatch(
-						output_strand, [=] {
-							cout << input_file
-							     << ":" << line_num
-							     << " " << line
-							     << endl;
-						});
-					    if (line_num % 10 == 0) {
-						    post(yield);
-					    }
-				    }
-			    });
-		}
-		thread_pool.join();
-	} catch (exception &e) {
-		cerr << "Exception: " << e.what() << endl;
+	if (argc < 2) {
+		cerr << "Usage: " << argv[0] << " <string> <files...>" << endl;
+		return EXIT_FAILURE;
 	}
+
+	boost::asio::thread_pool thread_pool;
+	boost::asio::strand<boost::asio::thread_pool::executor_type>
+	    output_strand(thread_pool.get_executor());
+
+	string search_string = argv[1];
+	for (int argn = 2; argn < argc; ++argn) {
+		string input_file = argv[argn];
+		boost::asio::spawn(
+		    thread_pool, [=](boost::asio::yield_context yield) {
+			    std::ifstream is(input_file.c_str());
+			    string line;
+			    size_t line_num = 0;
+			    while (getline(is, line)) {
+				    ++line_num;
+				    if (line.find(search_string) ==
+					string::npos) {
+					    continue;
+				    }
+
+				    boost::asio::dispatch(output_strand, [=] {
+					    cout << input_file << ":"
+						 << line_num << " " << line
+						 << endl;
+				    });
+
+				    if (line_num % 10 == 0) {
+					    post(yield);
+				    }
+			    }
+		    });
+	}
+
+	thread_pool.join();
 	return 0;
 }
