d822de1d8287b71457e483f81317db6012cff4c9
Jack Drogon <jack.xsuperman@gmail.com>
2023-03-15 09:21:42 +0800

    [rust] {basic} Update struct.rs

--- rust/basic/udt/struct.rs
+++ rust/basic/udt/struct.rs
@@ -4,9 +4,15 @@ struct Point {
     y: i32,
 }
 
+/// 匿名只有一个字段的结构体
+/// 感觉这样写还不如golang呢
+struct Structure(i32);
+
 fn main() {
     let point = Point { x: 0, y: 0 }; // point: Point
 
     println!("The point is at ({}, {})", point.x, point.y);
     println!("Point is {:?}", point); // {:?} need Debug trait
+    dbg!(std::mem::size_of::<Point>());
+    dbg!(std::mem::size_of::<Structure>());
 }
