afee358619382087978a43dc72cc01b746e598a6
Jack Drogon <jack.xsuperman@gmail.com>
2023-04-16 19:21:33 +0800

    [cpp] {extend-library} Refactor google-benchmark memcpy_by_sparse_range_bench

--- cpp/library/extend-library/google-benchmark/src/memcpy_by_sparse_range_bench.cc
+++ cpp/library/extend-library/google-benchmark/src/memcpy_by_sparse_range_bench.cc
@@ -1,4 +1,6 @@
-#include <cstdlib>
+#include <cstring>
+
+#include <memory>
 
 #include <benchmark/benchmark.h>
 
@@ -9,15 +11,15 @@
 // of memcpy() calls of different lengths:
 static void BM_memcpy(benchmark::State &state)
 {
-	char *src = new char[state.range(0)];
-	char *dst = new char[state.range(0)];
-	memset(src, 'x', state.range(0));
+	auto src = std::make_unique<char[]>(state.range(0));
+	auto dst = std::make_unique<char[]>(state.range(0));
+	memset(src.get(), 'x', state.range(0));
+
 	for (auto _ : state) {
-		memcpy(dst, src, state.range(0));
+		memcpy(dst.get(), src.get(), state.range(0));
 	}
+
 	state.SetBytesProcessed(state.iterations() * state.range(0));
-	delete[] src;
-	delete[] dst;
 }
 
 // default multiplier is 8
