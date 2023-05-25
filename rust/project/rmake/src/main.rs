13c5a186aae7fd9f7c100a96d758f8c6171ffb68
Jack Drogon <jack.xsuperman@gmail.com>
2023-05-25 21:17:13 +0800

    [rust] {project} Simpify rmake main
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- rust/project/rmake/src/main.rs
+++ rust/project/rmake/src/main.rs
@@ -4,28 +4,16 @@ use crate::lexer::Lexer;
 const RMAKEFILE: &str = "rmakefile";
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let mut target_name: Option<String> = None;
-    // if args.len() > 1 {
-    //     target_name = Some(args[1].clone());
-    // }
-
     // create lexer
     let mut lexer = Lexer::new(String::from(""), RMAKEFILE.to_string());
     loop {
         let token = lexer.next();
         match token {
-            lexer::Token::Assignment(line) => {
-                println!("Assignment: {}", line);
-            }
-            lexer::Token::Dependency(line) => {
-                println!("Dependency: {}", line);
-            }
-            lexer::Token::Task(line) => {
-                println!("Task: {}", line);
-            }
             lexer::Token::Eof => {
                 break;
+            }
+            _ => {
+                println!("{:?}", token)
             }
         }
     }
