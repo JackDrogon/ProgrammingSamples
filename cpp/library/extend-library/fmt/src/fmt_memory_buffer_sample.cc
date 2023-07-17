b782371f2db1d112f801b1c5016827e09e4d4012
Jack Drogon <jack.xsuperman@gmail.com>
2023-07-17 09:29:10 +0800

    [cpp] {extend-library} Update fmt_memory_buffer_sample
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- cpp/library/extend-library/fmt/src/fmt_memory_buffer_sample.cc
+++ cpp/library/extend-library/fmt/src/fmt_memory_buffer_sample.cc
@@ -1,16 +1,21 @@
+#include <iostream>
+#include <iterator>
+
 #include <fmt/format.h>
 
-#include <iostream>
 using namespace std;
 
 int main()
 {
-	fmt::memory_buffer m;
-	// TODO need \0
-	fmt::format_to(m, "Look, a {} string", 'C');
+	fmt::memory_buffer buffer;
+	// TODO need '\0'
+	// fmt::format_to(buffer, "Look, a {} string", 'C');
+	fmt::format_to(std::back_inserter(buffer), "For a moment, {} happened.",
+		       "nothing");
 
-	cout << fmt::to_string(m) << endl;
-	cout << m.data() << endl; // '\0'
+	cout << fmt::to_string(buffer) << endl;
+	cout << buffer.data() << endl; // '\0'
+	cout << buffer.size() << endl;
 
 	return 0;
 }
