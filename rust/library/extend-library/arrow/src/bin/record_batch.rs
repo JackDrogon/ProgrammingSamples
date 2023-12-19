4bef81be868826a0518472e269a8d64d6949e665
Jack Drogon <jack.xsuperman@gmail.com>
2023-12-19 21:14:17 +0800

    [rust] {extend-library} use data_type in record_batch.rs
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- rust/library/extend-library/arrow/src/bin/record_batch.rs
+++ rust/library/extend-library/arrow/src/bin/record_batch.rs
@@ -1,10 +1,10 @@
-use arrow_array::{Int32Array, RecordBatch};
-use arrow_schema::{DataType, Field, Schema};
+use arrow_array::{Array, Int32Array, RecordBatch};
+use arrow_schema::{Field, Schema};
 use std::sync::Arc;
 
 fn main() {
     let id_array = Int32Array::from(vec![1, 2, 3, 4, 5]);
-    let schema = Schema::new(vec![Field::new("id", DataType::Int32, false)]);
+    let schema = Schema::new(vec![Field::new("id", id_array.data_type().clone(), false)]);
 
     let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(id_array)]).unwrap();
     println!("{:?}", batch)
