6a789e1dd437f5c8c50509ec0b30c1748affb4cd
Jack Drogon <jack.xsuperman@gmail.com>
2023-12-30 03:21:19 +0800

    [cpp] {extend-library} Add boost coroutine2 fibonacci_bool_convert
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

---
+++ cpp/library/extend-library/boost/coroutine2/fibonacci_bool_convert.cc
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <boost/coroutine2/all.hpp>
+
+using pull_type_t = boost::coroutines2::coroutine<int>::pull_type;
+using push_type_t = boost::coroutines2::coroutine<int>::push_type;
+
+int main()
+{
+	pull_type_t source([](push_type_t &sink) {
+		int first = 1, second = 1;
+		sink(first);
+		sink(second);
+
+		for (int i = 0; i < 8; ++i) {
+			int third = first + second;
+			first = second;
+			second = third;
+
+			sink(third);
+		}
+	});
+
+	while (source) {
+		source(); // switch to coroutine push, like resume
+		auto i = source.get();
+		std::cout << i << " ";
+	}
+	std::cout << "\nDone" << std::endl;
+
+	return 0;
+}
