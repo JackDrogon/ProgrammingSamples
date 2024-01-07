4b13f48e3bda538b19955b9143c79a6e9dd959c1
Jack Drogon <jack.xsuperman@gmail.com>
2024-01-07 08:23:14 +0800

    [cpp] {codebase} Fix missing include/common/defer.hh
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

---
+++ cpp/codebase/include/common/defer.hh
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <memory>
+#include <type_traits>
+
+namespace archimedes::common
+{
+
+namespace defer_internal
+{
+
+template <typename Call> class Defer
+{
+public:
+	Defer(const Defer &) = delete;
+	Defer &operator=(const Defer &) = delete;
+
+	Defer(Call &call) : call_(call) {}
+	Defer(Call &&call) : call_(std::move(call)) {}
+	~Defer() { call_(); }
+
+private:
+	std::decay_t<Call> call_;
+};
+
+} // namespace defer_internal
+
+struct DeferKeywordWrapper {
+	template <typename Call> auto operator<<(Call &&call)
+	{
+		return defer_internal::Defer(std::forward<Call>(call));
+	}
+};
+
+} // namespace archimedes::common
+
+#define DEFER_CONCAT(a, b, c) a##_##b##_##c
+#define DEFER(line, counter)                                                   \
+	auto DEFER_CONCAT(_defer, line, counter) =                             \
+	    archimedes::common::DeferKeywordWrapper() <<
+#define defer DEFER(__LINE__, __COUNTER__)
+
+// clang-format off
+#define SCOPE_EXIT DEFER(__LINE__, __COUNTER__) [&]
+// clang-format on
