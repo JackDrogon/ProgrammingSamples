7c3a21c6e922dd8fffb0138cca79d452f57bbbdf
Jack Drogon <jack.xsuperman@gmail.com>
2024-01-24 19:23:45 +0800

    [cpp] {codebase} Refactor stop_watch example
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- cpp/codebase/examples/stop_watch.cc
+++ cpp/codebase/examples/stop_watch.cc
@@ -1,13 +1,13 @@
-#include "stop_watch.hh"
-
 #include <iostream>
+
+#include "archimedes/monitoring/stop_watch.hh"
 using namespace std;
 
 int main()
 {
 	archimedes::time::StopWatch watch;
 
-	volatile int n{0};
+	volatile int n{ 0 };
 	for (auto i = 0; i < 10'000'000; ++i) {
 		n += i;
 	}
